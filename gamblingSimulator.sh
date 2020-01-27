3be3fd25d43cafc189523568de3b511f22f21c83
admin <rajnish.kahar1996@gmail.com>
2020-01-27 18:36:36 +0530

    ADDED:Total Amount Won/Lost after 20days

--- gamblingSimulator.sh
+++ gamblingSimulator.sh
@@ -4,20 +4,32 @@ STAKE=100;
 BET=1;
 cash=$STAKE;
 calculativeAmount=$((($STAKE*50)/100));
+MONTH_DAYS=20;
+declare -A days
+totalAmount=0
 
 echo "Welcome to Gambler game";
 function bet()
 {
-if [[ $((RANDOM%2)) -eq 1 ]]
-then
-	cash=$((cash+$BET))
-else
-	cash=$((cash-$BET))
-fi
+	if [[ $((RANDOM%2)) -eq 1 ]]
+	then
+		cash=$((cash+$BET))
+	else
+		cash=$((cash-$BET))
+	fi
 }
 
-while [[ $cash -lt $(($STAKE+$calculativeAmount)) && $cash -gt $(($STAKE-$calculativeAmount)) ]]
+for (( i=1; i<=20; i++ ))
 do
-	bet
+	cash=$STAKE
+	dailyAmount=0;
+	while [[ $cash -lt $(($STAKE+$calculativeAmount)) && $cash -gt $(($STAKE-$calculativeAmount)) ]]
+	do
+		bet
+	done
+		dailyAmount=$(( $cash-$STAKE ))
+		days[day$i]=$dailyAmount;
+		totalAmount=$((totalAmount+${days[day$i]}));
 done
-   echo "Cash for the day: "$cash
+		echo "Total Amount Won/Loose after 20 days: "$totalAmount
+
