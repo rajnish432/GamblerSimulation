9680cf96066e11e8b42da3e8952b410a72240adc
admin <rajnish.kahar1996@gmail.com>
2020-01-27 14:45:48 +0530

    ADDED:Win and Loose conditions

--- gamblingSimulator.sh
+++ gamblingSimulator.sh
@@ -2,4 +2,16 @@
 
 STAKE=100;
 BET=1;
+cash=$STAKE;
+
 echo "Welcome to Gambler game";
+if [[ $((RANDOM%2)) -eq 1 ]]
+then
+	echo "You won";
+	((cash++))
+else
+	echo "You Loose";
+	((cash--))
+fi
+
+
